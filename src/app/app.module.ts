64b47460dd2aaa0b6f54b9e8b21bcf73e91daf6c
Jairo Reyes <ja.reyesr1@uniandes.edu.co>
2024-04-27 23:17:10 -0500

    Se agreó módulo de café

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CoffeeModule } from './coffee/coffee.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    CoffeeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
